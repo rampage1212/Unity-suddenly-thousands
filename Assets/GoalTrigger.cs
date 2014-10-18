59f9053767b4521140b0ca31d06597e05e2127ea
rampage0131 <jake.docker0131@gmail.com>
2014-10-18 13:11:44 -0400

    Setup the goal trigger to work.

--- Assets/GoalTrigger.cs
+++ Assets/GoalTrigger.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class GoalTrigger : MonoBehaviour {
+[RequireComponent(typeof(Collider))]
+public class GoalTrigger : MonoBehaviour
+{
+    public TextMesh numberIndicator;
+    public int expectedNumber = 1;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    readonly HashSet<Collider> characters = new HashSet<Collider>();
+
+    void Start()
+    {
+        numberIndicator.text = string.Format("0/{1}", 0, expectedNumber);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") == true)
+        {
+            characters.Add(other);
+            numberIndicator.text = string.Format("{0}/{1}", characters.Count, expectedNumber);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player") == true)
+        {
+            characters.Remove(other);
+            numberIndicator.text = string.Format("{0}/{1}", characters.Count, expectedNumber);
+        }
+    }
 }
