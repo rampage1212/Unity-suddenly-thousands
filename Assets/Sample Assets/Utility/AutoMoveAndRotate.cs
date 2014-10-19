687b424c85c2c00eb56ad2182a63fd4a526e18ce
rampage0131 <jake.docker0131@gmail.com>
2014-10-19 17:05:21 -0400

    Fixing menu and credits.

--- Assets/Sample Assets/Utility/AutoMoveAndRotate.cs
+++ Assets/Sample Assets/Utility/AutoMoveAndRotate.cs
@@ -6,6 +6,7 @@ public class AutoMoveAndRotate : MonoBehaviour {
 	public Vector3andSpace moveUnitsPerSecond;
 	public Vector3andSpace rotateDegreesPerSecond;
 	public bool ignoreTimescale;
+    public bool useFixedUpdate = false;
 	float lastRealTime;
 
 	void Start()
@@ -15,15 +16,30 @@ public class AutoMoveAndRotate : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float deltaTime = Time.deltaTime;
-		if (ignoreTimescale)
-		{
-			deltaTime = (Time.realtimeSinceStartup-lastRealTime);
-			lastRealTime = Time.realtimeSinceStartup;
-		}
-		transform.Translate (moveUnitsPerSecond.value * deltaTime, moveUnitsPerSecond.space);
-		transform.Rotate (rotateDegreesPerSecond.value * deltaTime, moveUnitsPerSecond.space);
+        if(useFixedUpdate == false)
+        {
+            Animate();
+        }
 	}
+
+    void FixedUpdate () {
+        if(useFixedUpdate == true)
+        {
+            Animate();
+        }
+    }
+
+    void Animate()
+    {
+        float deltaTime = Time.deltaTime;
+        if (ignoreTimescale)
+        {
+            deltaTime = (Time.realtimeSinceStartup - lastRealTime);
+            lastRealTime = Time.realtimeSinceStartup;
+        }
+        transform.Translate(moveUnitsPerSecond.value * deltaTime, moveUnitsPerSecond.space);
+        transform.Rotate(rotateDegreesPerSecond.value * deltaTime, moveUnitsPerSecond.space);
+    }
 
 	[System.Serializable]
 	public class Vector3andSpace
